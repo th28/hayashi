0f7ce4583b0cac5df55a51b609178119694845aa
Tom Holt <tomholt373@gmail.com>
2020-09-12 11:30:18 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-
-twitter_auth_keys = {
-        "api_key": "GJ87QDkiDUMsjG5Ru7sTVzwjR",
-        "api_secret"     : "ojMwI1TASpOKnA4gz8bt7WVYjjIgBSegt2Pzjktef7hBeK8ub1",
-        "access_token"        : "1302684037966901248-qjDqOR5oMzowtDQTYHZVgcqwZLS03d",
-        "access_token_secret" : "Tt1IdUfjV7J8E0HJEPl5nMVFhy2QP6aGYoML9hYwNuEmH"
-    }
-
